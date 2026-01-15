716f192d7886af112e0be4d3f1027a7dd89f345f
skendzierski12 <skendzierski@gmail.com>
2026-01-15 18:58:36 +0100

    Poprawki modeli, dodany admin panel

--- projekt_zaliczeniowy/wiki/admin.py
+++ projekt_zaliczeniowy/wiki/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Continent, Location, Race, Guild, Kingdom, Plague, Item
 
-# Register your models here.
+admin.site.register(Continent)
+admin.site.register(Location)
+admin.site.register(Race)
+admin.site.register(Guild)
+admin.site.register(Kingdom)
+admin.site.register(Plague)
+admin.site.register(Item)
--- projekt_zaliczeniowy/wiki/models.py
+++ projekt_zaliczeniowy/wiki/models.py
@@ -5,27 +5,38 @@ class Continent(models.Model):
     name = models.CharField(max_length = 100)
     description = models.TextField()
     climate = models.CharField(max_length = 100)
-    terrain_type = models.CharFied(max_length = 100)
+    terrain_type = models.CharField(max_length = 100)
+def __str__(self):
+    return self.name
 
 class Location(models.Model):
     name = models.CharField(max_length = 100)
     description = models.TextField()
+    continent = models.ForeignKey(Continent, on_delete=models.CASCADE)
     population = models.IntegerField()
+    location_type = models.CharField(max_length=50)
     is_safe_from_plague = models.BooleanField(default=False)
-    emblem = models.ImageField()
+    emblem = models.ImageField(upload_to='emblems/', blank=True, null=True)
+    
+def __str__(self):
+    return self.name
 
 class Race(models.Model):
     name = models.CharField(max_length= 100)
     description = models.TextField()
-    resistance = models.TextChoices()
+    resistance = models.TextField() #TextChoices()
     homeland = models.ForeignKey(Continent, on_delete=models.CASCADE)
     traits = models.TextField()
-    ethos = models.CharField(max_lenght = 1500)
+    ethos = models.TextField()
+def __str__(self):
+    return self.name
 
 class Guild(models.Model):
-    name = models.CharField(max_lenght = 100)
+    name = models.CharField(max_length = 100)
     description = models.TextField()
     special_traits = models.TextField()
+def __str__(self):
+    return self.name
 
 class Kingdom(models.Model):
     name = models.CharField(max_length = 100)
@@ -33,6 +44,8 @@ class Kingdom(models.Model):
     location = models.ForeignKey(Location, on_delete=models.CASCADE)
     population = models.IntegerField()
     homeland = models.ForeignKey(Continent, on_delete=models.CASCADE)
+def __str__(self):
+    return self.name
 
 class Plague(models.Model):
     description = models.TextField()
@@ -40,11 +53,14 @@ class Plague(models.Model):
     symptomps = models.TextField()
     death = models.IntegerField()
     epicentre = models.ForeignKey(Location, on_delete=models.CASCADE)
+def __str__(self):
+    return self.name
 
 
 class Item(models.Model):
-    type = models.ForeignKey(Item_type, on_delete=models.CASCADE)
-    name = models.CharField(max_lenght = 100)
+    name = models.CharField(max_length = 100)
     description = models.TextField()
+def __str__(self):
+    return self.name
 
 
